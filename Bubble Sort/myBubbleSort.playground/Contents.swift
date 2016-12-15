04fce6564b6b4d316205985290be210015bc7b6b
David Livingstone <david@david.tl>
2016-12-15 14:56:10 -0800

    bubble sort working

--- Bubble Sort/myBubbleSort.playground/Contents.swift
+++ Bubble Sort/myBubbleSort.playground/Contents.swift
@@ -2,27 +2,29 @@ import UIKit
 
 func bubbleSort(_ input: inout [Int]){
     
-    for index in 0..<input.count {
-        print("\(index): \(input[index])")
-        
-        
-        if index != input.count - 1 { //how do I repeat this? how do I know when I'm done?
-            //token thing?
-            if input[index] > input[index + 1] {
-                let temp = input[index]
-                input[index] = input[index + 1]
-                input[index + 1] = temp
-            }
-            
-            
-            print(input[index + 1])
-        } else {
-            print("at the end")
-        }
-        print("___")
-    }
+    var finished = false
     
+    while finished != true {
+        finished = true
+        
+        for index in 0..<input.count {
+            print("\(index): \(input[index])")
+            
+            if index != input.count - 1 {
+                
+                if input[index] > input[index + 1] {
+                    let temp = input[index]
+                    input[index] = input[index + 1]
+                    input[index + 1] = temp
+                    finished = false
+                }
+                print(input[index + 1])
+            } else if finished == true {
+                return
+            }
+        }
+    }
 }
 
-var foo = [6, 4, 1]
+var foo = [6, 4, 1, 4, 6, 2 ,35, 9, 0, 1]
 bubbleSort(&foo)
