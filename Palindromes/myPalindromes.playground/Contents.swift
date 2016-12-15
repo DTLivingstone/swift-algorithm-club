7dd4636a9d380caa3640e1dd9486b89ac24f75f7
David Livingstone <david@david.tl>
2016-12-15 12:19:50 -0800

    adjusting flow in palindromes

--- Palindromes/myPalindromes.playground/Contents.swift
+++ Palindromes/myPalindromes.playground/Contents.swift
@@ -2,30 +2,43 @@ import UIKit
 
 func palindromeTest(input: String) -> Bool {
     
-        if input.characters.count < 2 {
-            print("\(input) is NOT a palindrome")
-            return false
-        }
+    print("--------")
+    print(input)
     
-    let compareSteps = abs(input.characters.count / 2)
+    if input.characters.count < 2 {
+        print("too short!")
+        return false
+    } else {
+        let compareSteps = abs(input.characters.count / 2)
+        
+        for index in 0..<compareSteps {
+            print("character \(index) of \(input.characters.count - 1)")
+            let charFront = input[input.index(input.startIndex, offsetBy:index)]
+            let charBack = input[input.index(input.startIndex, offsetBy:(input.characters.count - 1 - index))]
+            print("\(charFront) <-> \(charBack)")
+            if charFront != charBack {
+                print("\(input) is NOT a palindrome")
+                return false
+            } else if index == compareSteps - 1 {
+                print("\(input) IS a palindrome")
+                return true
+            } else {
+                print("* * * * else statement")
+                return false
+            }
+            
+        } //make exhaustive?
+        
+    }
     
-    for index in 0..<compareSteps {
-        print("\(index) of \(input.characters.count)")
-        let charFront = input[input.index(input.startIndex, offsetBy:index)]
-        let charBack = input[input.index(input.startIndex, offsetBy:(input.characters.count - 1 - index))]
-        if charFront != charBack {
-            print("\(input) is NOT a palindrome")
-            return false
-        } else if index == compareSteps - 1 {
-            print("\(input) IS a palindrome")
-            return true
-        }
-    } //make exhaustive?
-    print("outside comparison")
+    print("* * * * outside comparison")
     return false
 }
 
 palindromeTest(input: "tacocat")
 palindromeTest(input: "pickles")
-palindromeTest(input: "t")
+palindromeTest(input: "too")
+palindromeTest(input: "tt")
 palindromeTest(input: "")
+palindromeTest(input: "t")
+palindromeTest(input: "ttt")
